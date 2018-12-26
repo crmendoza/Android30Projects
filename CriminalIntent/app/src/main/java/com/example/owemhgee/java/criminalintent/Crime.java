d1efd666620202dd94751c0d36f3df03b22fea89
crmendoza <c.roanne.mendoza@gmail.com>
2018-12-26 21:59:46 +0900

    First part of criminal intent project

---
+++ CriminalIntent/app/src/main/java/com/example/owemhgee/java/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package com.example.owemhgee.java.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+}
