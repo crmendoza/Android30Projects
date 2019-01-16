046953705587200a8c19349e4e0e46293c0d64e8
crmendoza <c.roanne.mendoza@gmail.com>
2019-01-16 21:35:20 +0900

    Add temporary click listener to CrimeHolder

--- CriminalIntent/app/src/main/java/com/example/owemhgee/java/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/owemhgee/java/criminalintent/CrimeListFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -42,7 +43,7 @@ public class CrimeListFragment extends Fragment {
 
     //private classes
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -52,6 +53,7 @@ public class CrimeListFragment extends Fragment {
         public CrimeHolder(View itemView) {
             super(itemView);
 
+            itemView.setOnClickListener(this);
             mTitleTextView = itemView.findViewById(R.id.list_item_crime_title_text_view);
             mDateTextView = itemView.findViewById(R.id.list_item_crime_date_text_view);
             mSolvedCheckBox = itemView.findViewById(R.id.list_item_crime_solved_check_box);
@@ -62,6 +64,14 @@ public class CrimeListFragment extends Fragment {
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getFormattedDate());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),
+                    mCrime.getTitle() + " clicked!",
+                    Toast.LENGTH_SHORT)
+                    .show();
         }
     }
 
